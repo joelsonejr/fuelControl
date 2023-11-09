2e6fc86767fc7a9d98a9dcf4b6f6e56cd8cccdc1
JoelsonJr <joelson.ejr@gmail.com>
2023-11-09 14:40:10 -0300

    Started detailing the business rules

---
+++ fuelControl.js
@@ -0,0 +1,15 @@
+/*
+I as an user of the app
+WANT to input my car model/ maker, millage, litters of fuel filled
+TO receive a report containing how much fuel I spent (comparisson between present x last month), 
+how many KM I can drive before a refuel is needed and how is my car KM/fuel ratio.
+
+DEVELOPING STEPS
+1. Retrive information from user
+2. Store retrieved information
+3. Do the math
+4 Return info
+
+*/
+
+"use strict";
